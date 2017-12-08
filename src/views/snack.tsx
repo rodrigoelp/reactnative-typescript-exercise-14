78bec87f7e46eb5fbf98a3bd9b34418e8cd93167
Rodrigo Landaeta <rlandaeta@cochlear.com>
2017-12-08 16:05:16 +1100

    Gave a better look to each snack

--- src/views/snack.tsx
+++ src/views/snack.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
+import { Button, Avatar } from "react-native-elements";
 import { IProduct } from "../models";
 
 interface ISnackProps {
@@ -15,21 +16,24 @@ class Snack extends React.Component<ISnackProps, ISnackState> {
     constructor(props: ISnackProps) {
         super(props);
 
-        this.state = { quantity: 0};
+        this.state = { quantity: 0 };
     }
 
     public render() {
         return (
-            <View style={{ flex: 1 }}>
-                <View style={{ alignSelf: "stretch", flexDirection: "row", marginHorizontal: 48, alignContent: "center" }}>
-                    <Text style={{ alignItems: "stretch", flex: 1 }}>
-                        {this.props.product.productName}
-                    </Text>
-                    <Text>{this.state.quantity}</Text>
-                </View>
-                <View style={{ alignSelf: "flex-end", flexDirection: "row", marginHorizontal: 48, alignContent: "center" }}>
-                    <Button title="⬆️" onPress={() => this.update(1)} />
-                    <Button title="⬇️" onPress={() => this.update(-1)} />
+            <View style={{ flex: 1, marginVertical: 24, marginHorizontal: 48, flexDirection: "row" }}>
+                <Avatar source={{ uri: this.props.product.photoUrl }} avatarStyle={{ borderRadius: 4 }} large={true} />
+                <View style={{ flex: 1, marginLeft: 16 }}>
+                    <View style={{ alignSelf: "stretch", flexDirection: "row", alignContent: "center" }}>
+                        <Text style={{ flex: 1, textAlignVertical: "bottom" }}>
+                            {this.props.product.productName}
+                        </Text>
+                        <Text style={{ fontSize: 24 }}>{this.state.quantity}</Text>
+                    </View>
+                    <View style={{ flex: 1, alignItems: "flex-end", flexDirection: "row", marginHorizontal: 48 }}>
+                        <Button style={{ flex: 1, minWidth: 45 }} borderRadius={4} title="+" onPress={() => this.update(1)} />
+                        <Button style={{ flex: 1, minWidth: 45 }} borderRadius={4} title="-" onPress={() => this.update(-1)} />
+                    </View>
                 </View>
             </View>
         );
